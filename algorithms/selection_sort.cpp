fb8a8df59550f0c6aec3172f1d82e11994f5d2e2
abhishekjiitr <abhi2254015@gmail.com>
2016-02-04 20:38:02 +0530

    No message, sorry

--- algorithms/selection_sort.cpp
+++ algorithms/selection_sort.cpp
@@ -7,7 +7,7 @@ void selection_sort(int array[], int n)
 		for ( int i = 0 ; i < n - 1 ; i++ )
 			{
 				min = array[i];
-
+				index = i;
 				for ( int j = i+1 ; j < n ; j++)
 					{
 						if ( array[j] < min )
@@ -22,6 +22,29 @@ void selection_sort(int array[], int n)
 			}
 		int cloc2 = clock();
 		int diff = cloc2 - cloc1;
-		cout << "Time taken: " << diff <<" seconds";
+		cout << "Time taken on array: " << diff <<" seconds";
 		//printArray(array, n);
+	}
+void selection_sort(node* head)
+	{
+		node* ptr, *ptr2, *index, *temp;
+		ptr = ptr -> next;
+		while ( ptr -> next != NULL && ptr -> next -> next != NULL )
+			{	
+				ptr2 = ptr -> next; // initially supposed as minimum element
+				index = ptr;
+				int min = ptr -> data
+				while ( ptr2 != NULL && ptr2 -> next != NULL )
+					{
+						if ( min > ptr2 -> data )
+							{
+								index = ptr2;
+								min = ptr2 -> data; 
+							}
+						// SWAP index and ptr
+							
+						ptr2 = ptr2 -> next;
+					}
+				
+			}
 	}
