b73d4bf215b3053d48fd97ba292e29698a76807f
abhishekjiitr <abhi2254015@gmail.com>
2016-02-04 19:00:21 +0530

    Changes in Main file

--- Main.cpp
+++ Main.cpp
@@ -16,20 +16,38 @@ void printArray(int* a, int n)
 			cout << a[i] << " ";
 		cout << endl;
 	}
+void printLinkedList(node* ptr)
+	{
+		if ( ptr == NULL )
+			return;
+		ptr = ptr -> link;
+		while ( ptr != NULL )
+			{
+				cout << ptr -> data << " ";
+				ptr = ptr -> link;
+			}
+		cout << endl;
+	}
 int main()
 	{
 		int N, random;
-		struct* head, *ptr;
+		node *head, *ptr;
 		cout << "Enter N:" << endl;
 		//cin >> N;
 		N = 10;
 		int array[N];
-		head = ptr;
-		ptr = new node();
+		head = new node();
+		head -> data = -1;
+		ptr = head;
 		for ( int i = 0 ; i < N ; i++ )
 			{
+				ptr -> link = new node();
 				random = rand() % 1000000000;
 				array[i] = random;
+				ptr -> link -> data = random;
+				ptr -> link-> link = NULL;
+				ptr = ptr -> link;
 			}
 		printArray(array, N);
+		printLinkedList(head);
 	}
